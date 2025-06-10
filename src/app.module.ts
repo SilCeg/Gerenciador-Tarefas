9cbb171c3d5a65cbfe09b74ad54cc92d0b25e1f0
unknown <gustavocs.contato@gmail.com>
2025-06-09 21:59:27 -0300

    projeto em nest iniciado

---
+++ src/app.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { TasksModule } from './tasks/tasks.module';
+import { TasksModule } from './tasks/tasks.module';
+
+@Module({
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost/task-manager'),
+    TasksModule,
+  ],
+})
+export class AppModule {}
